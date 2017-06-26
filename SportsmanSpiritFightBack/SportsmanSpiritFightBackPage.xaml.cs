df9c33efa6ed7faf2a399b1d338cf6628e0e94cf
Mainak8717 <Mainak@Mainaks-MacBook-Pro.local>
2017-06-26 08:59:32 +0530

    News prof

---
+++ SportsmanSpiritFightBack/SportsmanSpiritFightBackPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace SportsmanSpiritFightBack
+{
+	public partial class SportsmanSpiritFightBackPage : ContentPage
+	{
+		public SportsmanSpiritFightBackPage()
+		{
+			InitializeComponent();
+			var tapGestureRecognizer = new TapGestureRecognizer();
+			tapGestureRecognizer.NumberOfTapsRequired = 1;
+			tapGestureRecognizer.Tapped += (s, e) => {
+				OnLoginButtonClicked();
+			};
+			loginLbl.GestureRecognizers.Add(tapGestureRecognizer);		
+		}
+
+		void OnLoginButtonClicked()
+		{
+			Navigation.PushAsync(new SportsmanSpiritFightBack_LoginScreen());
+		}
+	}
+}
---
+++ SportsmanSpiritFightBack/SportsmanSpiritFightBack_LoginScreen.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SportsmanSpiritFightBack
+{
+	public partial class SportsmanSpiritFightBack_LoginScreen : ContentPage
+	{
+		public SportsmanSpiritFightBack_LoginScreen()
+		{
+			InitializeComponent();
+		}
+	}
+}
